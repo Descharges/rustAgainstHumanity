f0a04843e93f930c6835fec61d69ebf20eb3a9d2
Paul Caillier <paul.caillier@utbm.fr>
2021-02-09 16:17:20 +0100

    clear some stuff

--- rustAgainstHumanity/target/debug/deps/rustAgainstHumanity-a646d86acd5cafaf.d
+++
@@ -1,6 +0,0 @@
-/home/paul/Projets/rust_bullshit/rustAgainstHumanity/rustAgainstHumanity/target/debug/deps/rustAgainstHumanity-a646d86acd5cafaf: src/main.rs src/structures.rs
-
-/home/paul/Projets/rust_bullshit/rustAgainstHumanity/rustAgainstHumanity/target/debug/deps/rustAgainstHumanity-a646d86acd5cafaf.d: src/main.rs src/structures.rs
-
-src/main.rs:
-src/structures.rs:
--- rustAgainstHumanity/target/debug/rustAgainstHumanity.d
+++
@@ -1,1 +0,0 @@
-/home/paul/Projets/rust_bullshit/rustAgainstHumanity/rustAgainstHumanity/target/debug/rustAgainstHumanity: /home/paul/Projets/rust_bullshit/rustAgainstHumanity/rustAgainstHumanity/src/main.rs /home/paul/Projets/rust_bullshit/rustAgainstHumanity/rustAgainstHumanity/src/structures.rs
